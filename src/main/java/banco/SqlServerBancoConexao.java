df85db0fddeb0171d336a965728c384636741f20
Renan Alves <renan.rocha@sptech.school>
2024-06-13 20:26:03 -0300

    username

--- src/main/java/banco/SqlServerBancoConexao.java
+++ src/main/java/banco/SqlServerBancoConexao.java
@@ -12,7 +12,7 @@ public class SqlServerBancoConexao extends BancoConexaoBase {
     @Override
     @Bean(name = "sqlServerDataSource")
     public DataSource getDataSource() {
-        return criarDataSource("com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://34.197.44.86;databaseName=viatech", "viatech", "urubu100");
+        return criarDataSource("com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://34.197.44.86;databaseName=viatech", "sa", "urubu100");
     }
 
     @Override
